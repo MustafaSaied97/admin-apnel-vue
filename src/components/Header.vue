7aacb4d9cabbe7fa6d2e2d5aba3bcdf2f2bd79d1
mustafa saied <mustafa.saied@hmaserv.com>
2023-06-26 12:19:20 +0300

    admin

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon variant="text" @click.stop="$emit('toggleDrawer')"></v-app-bar-nav-icon>
+        <v-toolbar-title>My files</v-toolbar-title>
+        <v-spacer></v-spacer>
+          <v-text-field
+            hide-details
+            prepend-icon="mdi-magnify"
+            single-line
+          ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>  
+</template>
+
+<script setup>
+    const emit =defineEmits([
+        'toggleDrawer',
+    ])
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-navigation-drawer :modelValue="drawer" @update:modelValue="drawer = $event">
+        <v-sheet
+          color="grey-lighten-4"
+          class="pa-4"
+        >
+          <v-avatar
+            class="mb-4"
+            color="grey-darken-1"
+            size="64"
+          ></v-avatar>
+  
+          <div>john@google.com</div>
+        </v-sheet>
+  
+        <v-divider></v-divider>
+  
+        <v-list>
+          <v-list-item
+            v-for="[icon, text] in links"
+            :key="icon"
+            link
+          >
+            <template v-slot:prepend>
+              <v-icon>{{ icon }}</v-icon>
+            </template>
+  
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script setup>
+    const props=defineProps({
+        drawer:Boolean,
+    })
+    const links = [
+        ['mdi-inbox-arrow-down','Inbox'],
+        ['mdi-send', 'Send'],
+        ['mdi-delete', 'Trash'],
+        ['mdi-alert-octagon', 'Spam'],
+      ]
+</script>
+
+<style>
+
+</style>
